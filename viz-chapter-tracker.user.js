49eee9614bf5fede47fb988669f29c6803356be4
Aurange <11928004+TheAurange@users.noreply.github.com>
2024-06-22 20:02:29 -0500

    Update viz-chapter-tracker.user.js

--- viz-chapter-tracker.user.js
+++ viz-chapter-tracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name    [Viz] Chapter Tracker
 // @author  Aurange
-// @version 1.7
+// @version 1.8
 // @match   https://www.viz.com/account
 // @grant   window.close
 // ==/UserScript==
@@ -9,11 +9,13 @@
 "use strict";
 
 new MutationObserver(function(mutationList, observer){
-  let manga = document.querySelectorAll("div.o_sort_container > div.p-cs-tile"),
-      init = true;
+  let manga = document.querySelectorAll("div.o_sort_container > div.p-cs-tile");
 
   if(manga.length > 0){
     observer.disconnect();
+
+    let init = false,
+        pE = false;
 
     manga.forEach(e => {
       let title = e.querySelectorAll("a.o_chapters-link > div")[1].textContent,
@@ -24,17 +26,21 @@ new MutationObserver(function(mutationList, observer){
         let check = window.open(link, "_blank");
 
         if(!check){
-          if(init){
-            init = false;
+          if(!init){
+            init = true;
 
-            alert("Enable pop-ups for this site.");
+            alert("Enable pop-ups for this site and then refresh.");
           }
         }
-        else localStorage.setItem(title, chapter);
+        else{
+          localStorage.setItem(title, chapter);
+
+          pE = true;
+        }
       }
     });
 
-    window.close();
+    if(pE) window.close();
   }
 }).observe(document, {
   childList: true,
